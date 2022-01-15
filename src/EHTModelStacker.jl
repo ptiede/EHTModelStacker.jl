61671835d32dfc95d3e4b0178a74c606ad7b96fa
Paul Tiede <ptiede91@gmail.com>
2022-01-15 14:05:21 -0500

    updated loadrose to convert multiple files

--- src/EHTModelStacker.jl
+++ src/EHTModelStacker.jl
@@ -1,6 +1,7 @@
 module EHTModelStacker
 
-export ChainH5, SnapshotWeights, getsnapshot, lpdf, keys, getparam, restricttime
+export ChainH5, SnapshotWeights, getsnapshot, lpdf, keys, getparam,
+       restricttime, getsnapshot, getsnapshotdf
 using Distributions
 using HDF5
 using StatsFuns
@@ -10,7 +11,7 @@ using TupleVectors
 using LoopVectorization
 using DataFrames
 using ArraysOfArrays
-using SpecialFunctions: besselix, beta
+using SpecialFunctions: besselix, beta, erfcx, erfc
 using NPZ
 
 
@@ -62,17 +63,31 @@ end
 function lpdf(d::SnapshotWeights, chain::ChainH5)
     ls = 0.0
     #ind = rand(axes(flatview(chain.chain),2), d.batchsize)
-    schain = @view flatview(chain.chain)[:, 1:d.batchsize, :]
-    @inbounds @simd for i in eachindex(chain.times)
+    schain = flatview(chain.chain)[:, 1:d.batchsize, :]
+    buffer = zeros(size(schain, 2))
+    #println(chain.times)
+    @inbounds for i in eachindex(chain.times)
         csub = @view schain[:,:, i]
         tmp = 0.0
-        @inbounds for l in eachcol(csub)
-            #l = @view csub[:,i]
-            tmp += exp(Distributions.logpdf(d.transition,l) - Distributions.logpdf(d.prior, l))
+        ntot = 0
+        for j in axes(csub,2)
+            l = @view(csub[:,j])
+            #println(l)
+            buffer[j] = Distributions.logpdf(d.transition,l) - Distributions.logpdf(d.prior, l)
+            #@assert isfinite(buffer[j]) "WTF $l"
+            #ntot += 1
+            #a = exp(Distributions.logpdf(d.transition,l) - Distributions.logpdf(d.prior, l))
+            #println(a)
+            #@assert isfinite(a) "WTF $l"
+            #tmp += a
         end
-        ls += log(tmp/d.batchsize+eps(typeof(tmp)))
+        #println(mean(exp.(buffer)), " ", std(exp.(buffer)))
+        ls += logsumexp(buffer)
+        #ls += log(tmp)
+        #println(ls)
+        #println(ntot)
     end
-    return ls
+    return ls - length(chain.times)*log(d.batchsize)
 end
 
 
@@ -88,6 +103,23 @@ function ChainH5(filename::String, quant::Symbol, nsamples=2000)
     logz = read(fid["logz"])
 
     return ChainH5{typeof(chain), typeof(quant), typeof(times), typeof(logz)}(chain, quant, times, logz)
+end
+
+function ChainH5(filename::String, nsamples=2000)
+    chain = h5open(filename, "r") do fid
+        times = read(fid["time"])
+        params = read(fid["params"])
+        logz = read(fid["logz"])
+        k = sortperm(parse.(Int, last.(split.(keys(fid["params"]), "scan"))))
+        names = keys(fid["params"])[k]
+        quant = Symbol.(keys(fid["params"]["scan1"]))
+        chain = nestedview(zeros(length(quant), nsamples, length(names)),2)
+        for (i,n) in enumerate(names)
+            chain[i] = Array(hcat([params[n][String(k)][1:nsamples] for k in quant]...)')
+        end
+        ChainH5{typeof(chain), typeof(quant), typeof(times), typeof(logz)}(chain, quant, times, logz)
+    end
+
 end
 
 function ChainH5(filename::String, quant::NTuple{N,Symbol}, nsamples=2000) where {N}
@@ -118,6 +150,19 @@ function getparam(c::ChainH5, ps::NTuple{N,Symbol}) where {N}
     return ChainH5{typeof(chain), typeof(ps), typeof(c.times), typeof(c.logz)}(chain, ps, c.times, c.logz)
 end
 
+function getsnapshot(c::ChainH5, i::Int)
+    ChainH5{typeof(c.chain[i]), typeof(c.names), typeof(c.times[i]), typeof(c.logz[i])}(
+                c.chain[i],
+                c.names,
+                c.times[i],
+                c.logz[i],
+                )
+end
+
+function getsnapshotdf(c::ChainH5, i::Int)
+    df = DataFrame((c.names[n] => c.chain[i][n, :] for n in eachindex(c.names))...)
+    return c.times[i], df
+end
 
 
 function restricttime(c::ChainH5, tmin, tmax)
